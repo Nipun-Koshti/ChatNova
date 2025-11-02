de7783307fe3d006bfac4e239fd95e362e37fb16
Nipun <koshtinipun2002@gmail.com>
2025-11-02 15:43:43 +0530

    more controller added (user and chats)

--- Backend/src/services/user.service.ts
+++ Backend/src/services/user.service.ts
@@ -4,3 +4,11 @@ export const  findByIdUserService = async(userId:string)=>{
     return await UserModel.findById(userId);
 
 }
+
+export const getUsersService = async (userId: string) => {
+  const users = await UserModel.find({ _id: { $ne: userId } }).select(
+    "-password"
+  );
+
+  return users;
+};
